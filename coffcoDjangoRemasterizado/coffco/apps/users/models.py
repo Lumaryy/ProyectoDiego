3c29fd398a12e2699593062cfeb6ec29091109d4
Lumaryy <luisamariasancheztovar2006@gmail.com>
2024-07-10 16:03:28 -0500

    primer push

---
+++ coffcoDjangoRemasterizado/coffco/apps/users/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser #registrar y autenticar usuarios
+
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = []
